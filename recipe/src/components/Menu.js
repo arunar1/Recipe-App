af5053f1059aaeee7db429f94bdc7ddc3caf7975
arunar1 <arunarofficial00@gmail.com>
2023-09-24 13:58:12 +0530

    fetchitemadded

--- recipe/src/components/Menu.js
+++ recipe/src/components/Menu.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import {useEffect } from 'react'
 
 export default function Menu() {
-    // const [data,setdata]=useState()
-    useEffect((async)=>{
-        const API_URL='www.themealdb.com/api/json/v1/1/search.php?f=a'
-         let response=fetch(API_URL)
-         let data=response.json()
-         
+
+    useEffect(()=>{
+        const fetchdata=(async()=>{
+        const API_URL="https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+         let response=await fetch(API_URL)
+         let data= await response.json()
+         console.log(data)
+        })
+        fetchdata()
     },[])
   return (
     <div>
-        <h1></h1>
+        <h1>hoi</h1>
       
     </div>
   )
