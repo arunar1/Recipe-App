9c96ae1f2a2789668bc830421ac4ebdbb9a33b6e
arunar1 <arunarofficial00@gmail.com>
2023-09-24 18:38:31 +0530

    commit

--- recipe/src/components/Menu.js
+++ recipe/src/components/Menu.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import {useEffect } from 'react'
+import {useEffect,useState } from 'react'
 
 export default function Menu() {
-
-    useEffect(()=>{
-        const fetchdata=(async()=>{
+    const [menu,setmenu]=useState([])
+    const fetchdata=(async()=>{
         const API_URL="https://www.themealdb.com/api/json/v1/1/search.php?f=a"
          let response=await fetch(API_URL)
          let data= await response.json()
-         console.log(data)
+         setmenu(data.meals)
+         console.log(menu)
         })
+    useEffect(()=>{
         fetchdata()
     },[])
+    console.log(menu)
+    let menuImages=menu.map((item)=>{
+        return (
+            <img src={item.strMealThumb} />
+        )
+    })
+    
   return (
     <div>
-        <h1>hoi</h1>
+        {menuImages}
       
     </div>
   )
