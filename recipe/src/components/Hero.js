0314cd1df3d5a4865deb9c30771efa9cf522f152
arunar1 <arunarofficial00@gmail.com>
2023-10-16 16:35:37 +0530

    commit

--- recipe/src/components/Hero.js
+++ recipe/src/components/Hero.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function hero() {
+export default function Hero() {
   return (
-    <div className='hero-container'>
-      
+    <div className='hero'>
+      <img src='https://images.unsplash.com/photo-1625580917212-e86139d26e1c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNoaXZhfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'></img>
+      <div className='hero-content'>
+        <h1>Kottayil Temple</h1>
+        <p>Kottayil temple situated in the kozhikode which is famous shiva temple located in muchukunnu.</p>
+        
+      </div>
       
     </div>
   )
