42da2b13f87c20b4e06ccab94a1a62e25d04a8c7
arunar1 <arunarofficial00@gmail.com>
2023-10-18 23:51:17 +0530

    commit

--- recipe/src/components/FilteredDishes.js
+++ recipe/src/components/FilteredDishes.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function FilteredDishes(props) {
-  console.log(props.allmenucategories)
-  console.log(props.allmenus )
-  const showFilteredDishes=()=>{
-    alert("hi")
+ 
+console.log(props.allmenus)
+  
+  
+  const [filtereddishes,setfiltereddishes]=useState([])
+  const showFilteredDishes=(category)=>{
 
+  let filtereddish=props.allmenus.filter((item)=>{
+      console.log(item)
+      return item.strCategory===category
+    }).map((item)=>{
+      return(
+        <li className='normal'>
+          <img className='size1' src={item.strMealThumb}/>
+          <h2>{item.strMeal}</h2>
+        </li>
+      )
+    })
+    setfiltereddishes(filtereddish)
+    
 
   }
   const allcategories= props.allmenucategories.map((item)=>{
     return(
-      <li onClick={showFilteredDishes} className='categoryspecial'>{item.strCategory}</li>
+      <li onClick={()=>{showFilteredDishes(item.strCategory)}} className='categoryspecial'>{item.strCategory}</li>
     )
 
   })
 
+  console.log(filtereddishes)
 
+ 
   return (
     <div className="filtered-dishes">
       <div className="container">
@@ -29,6 +46,7 @@ export default function FilteredDishes(props) {
       </div>
       <div className="filtered-dishes-list">
         <ul className='flex listdec'>
+          {filtereddishes}
 
         </ul>
 
--- recipe/src/components/Menu.js
+++ recipe/src/components/Menu.js
@@ -1,45 +1,58 @@
 import React from 'react'
-import {useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from './Hero'
 import Special from './Special'
 import FilteredDishes from './FilteredDishes'
 export default function Menu() {
-    const [menu,setmenu]=useState([])
-    const [catemenu,setcatmenu]=useState([])
-    const fetchdata=(async()=>{
-        const API_URL="https://www.themealdb.com/api/json/v1/1/search.php?f=c"
-         let response=await fetch(API_URL)
-         let data= await response.json()
-         setmenu(data.meals)
-        //  console.log(menu)
-        })
+    const [menu, setmenu] = useState([])
 
-        const categorydata=(async()=>{
-            const API_URL="https://www.themealdb.com/api/json/v1/1/categories.php";
-            let respose=await fetch(API_URL)
-            
-            let data=await respose.json()
-            setcatmenu(data.categories)
-        })
-    useEffect(()=>{
+    const [loading, setloading] = useState(true)
+    const [catemenu, setcatmenu] = useState([])
+    const fetchdata = (async () => {
+        const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?f=c"
+        let response = await fetch(API_URL)
+        let data = await response.json()
+        setmenu(data.meals)
+        setloading(false)
+        //  console.log(menu)
+    })
+
+    const categorydata = (async () => {
+        const API_URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
+        let respose = await fetch(API_URL)
+
+        let data = await respose.json()
+        setcatmenu(data.categories)
+    })
+    useEffect(() => {
         fetchdata()
         categorydata()
-    },[])
+    }, [])
     console.log(menu)
-    let menuImages=menu.map((item)=>{
+    let menuImages = menu.map((item) => {
         return (
-           <div>
-            <img src={item.strMealThumb} />
-            <h2>{item.strCategory}</h2>
-           </div>
+            <div>
+                <img src={item.strMealThumb} />
+                <h2>{item.strCategory}</h2>
+            </div>
         )
     })
-    
-  return (
-    <div className='bg-color'>
-       <Hero/> 
-       <Special  specialmenu={menu}/>
-       <FilteredDishes allmenucategories={catemenu} allmenus={menu}/>
-    </div>
-  )
+
+    return (
+        <div className='bg-color'>
+            <Hero />
+            {!loading ? (<Special specialmenu={menu} />) :
+
+
+                (
+                    <div>
+                        
+                        <h2>
+
+                            LOADING</h2>
+                    </div>
+                )}
+            {!loading ? <FilteredDishes allmenucategories={catemenu} allmenus={menu} /> : null}
+        </div>
+    )
 }
