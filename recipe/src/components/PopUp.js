6a0b6a468740f7baa13647899190997c0041ee67
Arun <arunarofficial00@gmail.com>
2023-12-27 13:17:37 +0530

    popstyled

--- recipe/src/components/PopUp.js
+++ recipe/src/components/PopUp.js
@@ -5,13 +5,20 @@ function PopUp(props) {
   return (
     <div className='popUpContainer'>
         <div className='popUp'>
+        <div className="popimg">
+        <div>
+        <img src={props.item.strMealThumb}></img>
         <h1>{props.item.strMeal}</h1>
-        <p>{props.item.strCategory}</p>
-        <p>{props.item.strArea}</p>
-        <p>{props.item.strInstructions}</p>
+        </div>
+        </div>
+        <p>Category : &nbsp;{props.item.strCategory}</p>
+        <p>Place &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;{props.item.strArea}</p>
+        <p>About   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;{props.item.strInstructions}</p>
+        <div className="backbutton">
         <button onClick={()=>{
             props.setShowPopUp(false)
         }}>back</button>
+        </div>
     </div>
     </div>
   )
