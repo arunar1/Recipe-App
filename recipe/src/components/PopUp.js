0ab34f5595a85f4545217337dfe1606ff0db3539
Arun <arunarofficial00@gmail.com>
2024-01-14 15:42:38 +0530

    styled pop up

--- recipe/src/components/PopUp.js
+++ recipe/src/components/PopUp.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function PopUp(props) {
-    console.log(props)
+  console.log(props);
   return (
-    <div className='popUpContainer'>
-        <div className='popUp'>
+    <div className="popUpContainer">
+      <div className="popUp">
         <div className="popimg">
-        <div>
-        <img src={props.item.strMealThumb}></img>
-        <h1>{props.item.strMeal}</h1>
-        </div>
+          <div>
+            <img src={props.item.strMealThumb}></img>
+            <h1>{props.item.strMeal}</h1>
+          </div>
         </div>
         <p>Category : &nbsp;{props.item.strCategory}</p>
-        <p>Place &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;{props.item.strArea}</p>
-        <p>About   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: &nbsp;{props.item.strInstructions}</p>
+        <p>
+          Place &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;
+          {props.item.strArea}
+        </p>
+        <p className="about">
+          About &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:<div> &nbsp;
+          {props.item.strInstructions}</div>
+        </p>
         <div className="backbutton">
-        <button onClick={()=>{
-            props.setShowPopUp(false)
-        }}>back</button>
+          <button
+            onClick={() => {
+              props.setShowPopUp(false);
+            }}
+          >
+            back
+          </button>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default PopUp
+export default PopUp;
