4bdff35d69c2108983331b7f9bcea38f9b477bab
Arun <arunarofficial00@gmail.com>
2024-06-13 22:04:30 +0530

    style: added style

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -11,9 +11,10 @@ export default function Checkout(props) {
    const item= props.cart.map((item)=>{
         return(
             <div className='checkoutCard'>
-            <img src={item.strMealThumb} />
-            <h5>{item.strMeal}</h5>
-            <Counter/>
+                <img src={item.strMealThumb} />
+                <h5>{item.strMeal}</h5>
+                <Counter/>
+                <button className='buttondlt'>Delete</button>
             </div>
         )
     })
@@ -24,9 +25,9 @@ export default function Checkout(props) {
     </div>
      <div className="checkoutbtn">
 
-     {props.cart.length > 0 ? <div className="buybtn" onClick={handleBuy}>
+     {props.cart.length > 0 ? <button className="buybtn" onClick={handleBuy}>
        Buy
-     </div> : <h1 className='empty-head'>Cart is empty</h1>}
+     </button> : <h1 className='empty-head'>Cart is empty</h1>}
     </div>
 
 
